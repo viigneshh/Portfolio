4f2e00a6841e4ac37e9a3633598690af754ff3a2
vignesh <maninagavignesh@gmail.com>
2025-07-14 13:04:07 +0530

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/', // 👈 matches your repo name!
   plugins: [react()],
-});
+})
