a95ceea667080cdb50b958d9034196ee33415649
vignesh <maninagavignesh@gmail.com>
2025-07-21 16:14:22 +0530

    analysis enabled through google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-//import Home from './pages/home'
-//import './App.css'
-import NavBar from "./components/navbar"
-import Aboutme from "./pages/about"
-import { Routes,Route } from "react-router-dom"
-import Home from "./pages/home"
-import Projects from "./pages/projects"
-import Contact from "./pages/contact"
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
+import usePageTracking from "./hooks/usePackagetracker";
+
+import NavBar from "./components/navbar";
+import Aboutme from "./pages/about";
+import Home from "./pages/home";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
+
+ReactGA.initialize("G-PQHELWXZ0E"); 
 
 function App() {
- 
+  usePageTracking(); // start tracking
 
   return (
     <div>
-      <NavBar/>
+      <NavBar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<Aboutme/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Aboutme />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-      
-    </div> 
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
